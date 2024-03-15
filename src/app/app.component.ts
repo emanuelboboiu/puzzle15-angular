2abd8b18beadc7990f70a4f909df56f0ccb4090a
emanuelboboiu <emanuelboboiu@gmail.com>
2024-03-15 17:13:01 +0200

    Second commit. At the beginnings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { NgFor, NgIf } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { RequestsService } from './requests.service';
+
 
 @Component({
   selector: 'app-root',
@@ -11,19 +13,22 @@ import { Component, NgModule } from '@angular/core';
 export class AppComponent {
   title = '15 Puzzle';
   currSection: number = 0; // 0 means main page, 1 means the game zone etc.
+  boardSize: number = 4;
 
   constructor() {
 
   } // end constructor.
 
-  // A method used to go to game board zone.
-  goToGame(): void {
-    this.currSection = 1;
-  } // end goToGame() method.
-
   // A method used to go to main page.
   goToMain(): void {
     this.currSection = 0;
   } // end goToMain() method.
+
+  // A method used to go to game board zone.
+  goToGame(size: number): void {
+    this.boardSize = size; // the size comes from the html button clickedf.
+    this.currSection = 1;
+
+  } // end goToGame() method.
 
 } // end class.

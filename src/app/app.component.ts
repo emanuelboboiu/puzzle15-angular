43ca2ce7e16b41fb8691e88b9a68c1b0fc642ca3
emanuelboboiu <emanuelboboiu@gmail.com>
2024-03-15 16:02:10 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'puzzle15';
-}
+  title = '15 Puzzle';
+  currSection: number = 0; // 0 means main page, 1 means the game zone etc.
+
+  constructor() {
+
+  } // end constructor.
+
+  // A method used to go to game board zone.
+  goToGame(): void {
+    this.currSection = 1;
+  } // end goToGame() method.
+
+  // A method used to go to main page.
+  goToMain(): void {
+    this.currSection = 0;
+  } // end goToMain() method.
+
+} // end class.

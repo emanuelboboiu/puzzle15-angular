f10463ac21a6dd2a34292c5a3a158d607e33c55b
emanuelboboiu <emanuelboboiu@gmail.com>
2024-04-03 15:38:45 +0300

    Finished 1.1 version.

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SettingsService {
-  isDev = true; // not to have many stats when developing.
+  isDev = false; // not to have many stats when developing.
   os: number = 1; // 0 means web, 1 means iOS, 2 means Android.
   language: string = "en"; // this is only to have something declared, never used this value.
   acceptedLanguages: string[] = ['en', 'ro']; // Array of accepted languages

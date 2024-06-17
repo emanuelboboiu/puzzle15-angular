bcf91e947c923b3b85ba6520ab934c2784a9011b
emanuelboboiu <emanuelboboiu@gmail.com>
2024-06-18 00:21:44 +0300

    Updating to Angular 18.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from "@angular/common/http";
+import {} from "@angular/common/http";
 import { importProvidersFrom } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from './app/app.config';
